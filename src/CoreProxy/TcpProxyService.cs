fa63ed00ae90daee4edb1f8e5a93e928c3a9f7b2
jnm2 <me@jnm2.com>
2016-08-05 17:22:49 -0400

    Handled connection reset and proxy shutdown

--- src/CoreProxy/TcpProxyService.cs
+++ src/CoreProxy/TcpProxyService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -10,6 +11,7 @@ namespace jnm2.CoreProxy
     {
         private readonly IPEndPoint to;
         private readonly TcpListener listener;
+        private CancellationTokenSource disposalSource = new CancellationTokenSource();
 
         public TcpProxyService(IPEndPoint from, IPEndPoint to)
         {
@@ -25,71 +27,111 @@ namespace jnm2.CoreProxy
 
         private void SubscribeToNextConnection()
         {
-            listener.AcceptTcpClientAsync().ContinueWith(ClientConnected, CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+            Task<TcpClient> acceptTask;
+            try
+            {
+                acceptTask = listener.AcceptTcpClientAsync();
+            }
+            catch (InvalidOperationException) when (disposalSource.IsCancellationRequested) // Handles race condition
+            {
+                return;
+            }
+
+            acceptTask.ContinueWith(ClientConnected, CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
         }
 
         private async void ClientConnected(Task<TcpClient> task)
         {
             SubscribeToNextConnection();
 
-            using (var clientConnection = task.GetAwaiter().GetResult())
+            TcpClient clientConnection;
+            try
+            {
+                clientConnection = task.GetAwaiter().GetResult();
+            }
+            catch (ObjectDisposedException) when (disposalSource.IsCancellationRequested) // Handles race condition
+            {
+                return;
+            }
+
+            using (clientConnection)
             using (var serverConnection = new TcpClient()) // TODO: pool
             {
+                if (disposalSource.IsCancellationRequested) return;
                 await serverConnection.ConnectAsync(to.Address, to.Port); // TODO: possibly pool?
                 
                 using (var serverStream = serverConnection.GetStream())
                 using (var clientStream = clientConnection.GetStream())
                 {
-                    // TODO: Pool buffers
-                    // TODO: Careful with cancellation. If one closes, flush before closing the other.
-
-                    await Task.WhenAll(
-                        new HalfDuplex(clientStream, serverStream, new byte[4096], new byte[4096]).Run(CancellationToken.None),
-                        new HalfDuplex(serverStream, clientStream, new byte[4096], new byte[4096]).Run(CancellationToken.None)); 
+                    try
+                    {
+                        await Task.WhenAll(
+                            new HalfDuplex(clientStream, serverStream, new byte[4096], new byte[4096]).Run(disposalSource.Token), // TODO: Pool buffers
+                            new HalfDuplex(serverStream, clientStream, new byte[4096], new byte[4096]).Run(disposalSource.Token));
+                    }
+                    catch (OperationCanceledException)
+                    {
+                    }
                 }
             }
         }
 
         private sealed class HalfDuplex
         {
-            private byte[] buffer1, buffer2;
+            private byte[] readBuffer, writeBuffer;
             private readonly NetworkStream fromStream, toStream;
 
             public HalfDuplex(NetworkStream fromStream, NetworkStream toStream, byte[] buffer1, byte[] buffer2)
             {
-                this.buffer1 = buffer1;
-                this.buffer2 = buffer2;
+                readBuffer = buffer1;
+                writeBuffer = buffer2;
                 this.fromStream = fromStream;
                 this.toStream = toStream;
             }
 
             public async Task Run(CancellationToken cancellationToken)
             {
-                var bytesRead = 0;
+                var bytesRead = await fromStream.ReadAsync(readBuffer, 0, readBuffer.Length, cancellationToken);
 
-                while (true)
+                while (bytesRead != 0)
                 {
-                    if (bytesRead != 0)
+                    var flip = writeBuffer;
+                    writeBuffer = readBuffer;
+                    readBuffer = flip;
+                    
+                    try
                     {
-                        var writeTask = toStream.WriteAsync(buffer1, 0, bytesRead, cancellationToken);
-                        var readTask = fromStream.ReadAsync(buffer2, 0, buffer2.Length, cancellationToken);
-                        await Task.WhenAll(writeTask, readTask);
-                        bytesRead = readTask.GetAwaiter().GetResult();
+                        var writeTask = toStream.WriteAsync(writeBuffer, 0, bytesRead, cancellationToken);
+                        var readTask = fromStream.ReadAsync(readBuffer, 0, readBuffer.Length, cancellationToken);
+                        await writeTask;
+                        bytesRead = await readTask;
                     }
-                    else
+                    catch (IOException ex) when (IsExpectedShutdownException(ex))
                     {
-                        bytesRead = await fromStream.ReadAsync(buffer2, 0, buffer2.Length, cancellationToken);
+                        break;
                     }
+                }
+            }
 
-                    var flip = buffer1;
-                    buffer1 = buffer2;
-                    buffer2 = flip;
+            private static bool IsExpectedShutdownException(IOException ex)
+            {
+                var socketException = ex.InnerException as SocketException;
+                if (socketException == null) return false;
+
+                switch (socketException.SocketErrorCode)
+                {
+                    case SocketError.ConnectionAborted:
+                    case SocketError.ConnectionReset: // Happens e.g. when a browser navigates away from a page using SignalR
+                        return true;
+                    default:
+                        return false;
                 }
             }
         }
 
         public void Dispose()
         {
+            disposalSource.Cancel();
             listener.Stop();
         }
     }
